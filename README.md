bad50708cb70bcf2975bf624b4b8b2a3dc065b1a
huanxi <huanxiqiao@qq.com>
2024-08-05 20:21:30 +0800

    feature: 输出分析结果文件

---
+++ README.md
@@ -0,0 +1,40 @@
+pm-package-pulse 是一个 npm 包依赖分析工具。它可以递归遍历 node_modules 文件夹，分析项目的依赖结构，并检测潜在的问题，如循环依赖、菱形依赖和多版本依赖，输出JSON格式的分析结果
+
+```bash
+npm install -g npm-package-pulse
+```
+
+```json
+{
+  //循环依赖检测：基于DFS的图遍历
+  "cyclicDependencies": [
+    ["packageA@1.0.0", "packageB@2.0.0", "packageC@1.5.0", "packageA@1.0.0"]
+  ],
+  //菱形依赖检测：广度优先搜索（BFS）
+  "diamondDependencies": [
+    {
+      "start": "packageA@1.0.0",
+      "end": "packageD@3.0.0",
+      "paths": [
+        ["packageA@1.0.0", "packageB@2.0.0", "packageD@3.0.0"],
+        ["packageA@1.0.0", "packageC@1.5.0", "packageD@3.0.0"]
+      ]
+    }
+  ],
+  //多版本依赖检测：哈希表版本统计
+  "multipleVersions": [
+    {
+      "name": "packageD",
+      "versions": ["3.0.0", "3.1.0"]
+    }
+  ]
+}
+```
+
+为了保证包的简化和算法扩展性，采用前后端分离的方式，将可视化代码单独部署在 `Vercel` 上，上传 JSON 文件得到如下渲染结果
+
+a. **分页懒加载渲染**：采用分页懒加载技术，将依赖包数据分页加载到页面中，以优化页面性能和加载速度。<br/>
+b. **交互式可视化**：用户可以通过与页面交互（如滚动浏览器窗口和搜索依赖名称）来浏览包的依赖关系，使得依赖关系的探索更加直观和交互性。
+
+![查看当前项目的依赖信息](docs/渲染.gif '查看当前项目的依赖信息')
+![搜索特定包的依赖信息](docs/搜索.gif '搜索特定包的依赖信息') 
--- src/index.js
+++ src/index.js
@@ -7,13 +7,20 @@ import { detectMultipleVersions } from './multiVersionDeps/multipleVersion.js';
 
 export function packagePulse(dir) {
 
+  const dataDir = path.join(dir, 'NPMPulse');
+  if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir);
+  }
+
   const dependencyTree = makeDepTree(dir);
   const diamondDependencies = detectDiamondDeps(dependencyTree);
   const circularDependencies = detectCircularDeps_DFS(dependencyTree);
   const multipleVersionDependencies = detectMultipleVersions(dependencyTree);
 
-  fs.writeFileSync(path.join(dir, 'dependencyTree.json'), JSON.stringify(dependencyTree, null, 2));
-  fs.writeFileSync(path.join(dir, 'diamondDependencies.json'), JSON.stringify(diamondDependencies, null, 2));
-  fs.writeFileSync(path.join(dir, 'circularDependencies.json'), JSON.stringify(circularDependencies, null, 2));
-  fs.writeFileSync(path.join(dir, 'multipleVersionDependencies.json'), JSON.stringify(multipleVersionDependencies, null, 2));
+  fs.writeFileSync(path.join(dataDir, 'dependencyTree.json'), JSON.stringify(dependencyTree, null, 2));
+  fs.writeFileSync(path.join(dataDir, 'diamondDependencies.json'), JSON.stringify(diamondDependencies, null, 2));
+  fs.writeFileSync(path.join(dataDir, 'circularDependencies.json'), JSON.stringify(circularDependencies, null, 2));
+  fs.writeFileSync(path.join(dataDir, 'multipleVersionDependencies.json'), JSON.stringify(multipleVersionDependencies, null, 2));
+
+  return "Dependency analysis is complete, please view the results in the folder 'NPMPulse'"
 }
